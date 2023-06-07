10d6c7de003294f24133e1e9bfcad55b3013fc61
Shawn Dibble <shawndibble@gmail.com>
2023-06-07 14:13:09 -0500

    handle undefined send to camelToPascal

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,5 +1,6 @@
 export function camelToPascal(text) {
-  const word = text.replace(/([A-Z])/g, ' $1').trim();
+  const word = text?.replace(/([A-Z])/g, ' $1').trim();
+  if (!word) return '';
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
