48cdebcf370b1adb9041c841f0e5029192f6658e
Shawn Dibble <shawndibble@gmail.com>
2023-07-04 10:54:22 -0500

    styling to custom tile help

---
+++ src/components/CustomTileDialog/CustomTileHelp/index.jsx
@@ -0,0 +1,103 @@
+import { styled } from '@mui/material/styles';
+import MuiAccordion from '@mui/material/Accordion';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+export default function CustomTileHelp() {
+  const [expanded, setExpanded] = useState(false);
+  const handleChange = (panel) => (_event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary aria-controls="panel1-content" id="panel1-header">
+          <Typography>Custom Tiles Explained</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography sx={{ mb: 2 }}>
+            Custom tiles let you add your own variations to the game board.
+            Utilize this dialog to add and remove tiles as you see fit.
+          </Typography>
+          <Typography sx={{ mb: 2 }}>
+            Custom tiles are added based on the kink and intensity you pick from
+            the drop down. As such, you need to have that kink and intensity
+            (or a higher intensity) selected for your custom tiles to show on the board.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <AccordionSummary aria-controls="panel2-content" id="panel2-header">
+          <Typography>Ideas & Suggestions</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="h6">Add new Activities</Typography>
+          <Typography variant="subtitle2" sx={{ mb: 2 }}>
+            Come up with new activities that are not part of
+            the existing list
+          </Typography>
+
+          <Typography variant="h6">Add harder tiles to easier intensities</Typography>
+          <Typography variant="subtitle2" sx={{ mb: 1 }}>
+            When you pick higher
+            intensity levels, the game will use the earlier intensity level and gradual move
+            to whatyou picked. If you want harder tiles early on, add several to lower
+            intensities.
+          </Typography>
+          <Typography variant="subtitle2" sx={{ mb: 2 }}>
+            Example: You play Poppers - Advanced. Add advanced tiles to Poppers - Beginner
+          </Typography>
+
+          <Typography variant="h6">Combine activities into a single tile</Typography>
+          <Typography variant="subtitle2" sx={{ mb: 1 }}>
+            Combine multiple activites together in a single tile that normally would not
+            go together.
+          </Typography>
+          <Typography variant="subtitle2">
+            Example: Spit roast. Use a toy in your throat and ass at the same time.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
+}
--- src/components/CustomTileDialog/index.jsx
+++ src/components/CustomTileDialog/index.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react';
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Dialog, DialogContent, DialogTitle, Divider, IconButton, Typography,
+  Dialog, DialogContent, DialogTitle, Divider, IconButton,
 } from '@mui/material';
-import { Close, ExpandMore } from '@mui/icons-material';
+import { Close } from '@mui/icons-material';
 import useWindowDimensions from 'hooks/useWindowDimensions';
 import useLocalStorage from 'hooks/useLocalStorage';
 import ToastAlert from 'components/ToastAlert';
 import AddCustomTile from './AddCustomTile';
+import CustomTileHelp from './CustomTileHelp';
 import ViewCustomTiles from './ViewCustomTiles';
 
 export default function CustomTileDialog({ setOpen, open = false }) {
@@ -46,69 +44,20 @@ export default function CustomTileDialog({ setOpen, open = false }) {
           </IconButton>
         </DialogTitle>
         <DialogContent>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMore />}
-              aria-controls="panel1-content"
-              id="panel1-header"
-            >
-              <Typography>Custom Tiles Explained</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography sx={{ mb: 2 }}>
-                Custom tiles let you add your own variations to the game board.
-                Utilize this dialog to add and remove tiles as you see fit.
-              </Typography>
-              <Typography sx={{ mb: 2 }}>
-                Custom tiles are added based on the kink and intensity you pick from
-                the drop down. As such, you need to have that kink and intensity
-                (or a higher intensity) selected for your custom tiles to show on the board.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMore />}
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              <Typography>Ideas & Suggestions</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="h6">Add new Activities</Typography>
-              <Typography variant="subtitle2" sx={{ mb: 2 }}>
-                Come up with new activities that are not part of
-                the existing list
-              </Typography>
+          <CustomTileHelp />
 
-              <Typography variant="h6">Add harder tiles to easier intensities</Typography>
-              <Typography variant="subtitle2" sx={{ mb: 1 }}>
-                When you pick higher
-                intensity levels, the game will use the earlier intensity level and gradual move
-                to whatyou picked. If you want harder tiles early on, add several to lower
-                intensities.
-              </Typography>
-              <Typography variant="subtitle2" sx={{ mb: 2 }}>
-                Example: You play Poppers - Advanced. Add advanced tiles to Poppers - Beginner
-              </Typography>
-
-              <Typography variant="h6">Combine activities into a single tile</Typography>
-              <Typography variant="subtitle2" sx={{ mb: 1 }}>
-                Combine multiple activites together in a single tile that normally would not
-                go together.
-              </Typography>
-              <Typography variant="subtitle2" sx={{ mb: 2 }}>
-                Example: Spit roast. Use a toy in your throat and ass at the same time.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
           <AddCustomTile
             setSubmitMessage={setSubmitMessage}
             addCustomTile={addCustomTile}
             customTiles={customTiles}
           />
-          {!!customTiles.length && (<Divider light sx={{ my: 2 }} />)}
-          <ViewCustomTiles customTiles={customTiles} setCustomTiles={setCustomTiles} />
+
+          {!!customTiles.length && (
+            <>
+              <Divider light sx={{ my: 2 }} />
+              <ViewCustomTiles customTiles={customTiles} setCustomTiles={setCustomTiles} />
+            </>
+          )}
         </DialogContent>
       </Dialog>
       <ToastAlert
