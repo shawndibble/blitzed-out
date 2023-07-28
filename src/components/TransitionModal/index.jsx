1965a5772cad322edf16370884860959cd8a0cd4
Shawn Dibble <shawndibble@gmail.com>
2023-07-28 14:44:54 -0500

    add countdown for dialog

--- src/components/TransitionModal/index.jsx
+++ src/components/TransitionModal/index.jsx
@@ -5,6 +5,7 @@ import Fade from '@mui/material/Fade';
 import Typography from '@mui/material/Typography';
 import { Trans, useTranslation } from 'react-i18next';
 import { extractAction } from 'helpers/strings';
+import { useEffect, useState } from 'react';
 
 const style = (theme) => ({
   position: 'absolute',
@@ -25,9 +26,29 @@ export default function TransitionModal({
   open, text, displayName, handleClose,
 }) {
   const { t } = useTranslation();
-
   const title = text?.match(/(?:#[\d]*:).*(?=\r)/gs);
   const description = extractAction(text);
+
+  const [timeLeft, setTimeLeft] = useState(8);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      setTimeLeft(null);
+    }
+
+    // exit early when we reach 0
+    if (!timeLeft) return;
+
+    // save intervalId to clear the interval when the
+    // component re-renders
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    // clear interval on re-render to avoid memory leaks
+    // eslint-disable-next-line consistent-return
+    return () => clearInterval(intervalId);
+  }, [timeLeft]);
 
   return (
     <div>
@@ -52,7 +73,7 @@ export default function TransitionModal({
               {description}
             </Typography>
             <br />
-            <Typography variant="caption"><Trans i18nKey="autoCloseModal" /></Typography>
+            <Typography variant="caption"><Trans i18nKey="autoCloseModal" values={{ timeLeft }} /></Typography>
           </Box>
         </Fade>
       </Modal>
