9a6fa141819c648a6452a071f7b897a29230c106
Shawn Dibble <shawndibble@gmail.com>
2023-07-28 18:13:15 -0500

    fix voice changing issue

--- src/services/textToSpeech.js
+++ src/services/textToSpeech.js
@@ -1,9 +1,11 @@
 import languages from 'locales/languages.json';
 
 export default function speak(message, language) {
-  const utterance = new SpeechSynthesisUtterance();
-  utterance.text = message;
-  utterance.voice = window.speechSynthesis.getVoices()
-    .find((v) => v.name === languages[language].voice);
-  setTimeout(() => window.speechSynthesis.speak(utterance), 700);
+  setTimeout(() => {
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.voice = window.speechSynthesis.getVoices()
+      .find((v) => v.name === languages[language].voice);
+    utterance.text = message;
+    window.speechSynthesis.speak(utterance);
+  }, 700);
 }
