b59e80d4a78c8ab77783eaad61d0505f06cd1106
Shawn Dibble <shawn.dibble@quartzy.com>
2023-08-20 11:01:15 -0500

    Have import/export output the game board. GH-39

--- src/views/ImportExport/index.jsx
+++ src/views/ImportExport/index.jsx
@@ -12,66 +12,103 @@ import {
 } from '@mui/material';
 import CloseIcon from 'components/CloseIcon';
 import ToastAlert from 'components/ToastAlert';
+import useAuth from 'hooks/useAuth';
 import useLocalStorage from 'hooks/useLocalStorage';
 import { useEffect, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
+import { sendMessage } from 'services/firebase';
 
 export default function ImportExport({ open, close, isMobile }) {
   const { t } = useTranslation();
   const [localGameBoard, setLocalGameBoard] = useLocalStorage('customBoard');
-  const { room, roomTileCount } = useLocalStorage('gameSettings')[0];
+  const settings = useLocalStorage('gameSettings')[0];
   const [textValue, setTextField] = useState('');
   const [alert, setAlert] = useState('');
+  const { user } = useAuth();
 
-  // default is 40 tiles, but we don't count the start and finish tiles.
-  // Also, why we sub 2 from roomTileCount.
-  const requiredTiles = room === 'public' ? 38 : roomTileCount - 2;
+  // default is 40 tiles. If room is private, use roomTileCount.
+  const { room, roomTileCount } = settings;
+  const requiredTiles = room === 'public' ? 40 : roomTileCount;
+
+  async function createGameMessage(gameTiles, importLabel) {
+    const gameTileTitles = gameTiles.map(({ title }) => `* ${title}`);
+    // remove our start and finish tiles from the list.
+    gameTileTitles.pop();
+    gameTileTitles.shift();
+
+    let message = '### Imported a custom board.\n\n';
+    if (importLabel) {
+      message += `Title: ${importLabel.join('\n')}\n\n`;
+    }
+    message += 'Board includes the following categories:\n';
+    message += [...new Set(gameTileTitles)].join('\n');
+
+    await sendMessage({
+      room: room || 'public',
+      user,
+      text: message,
+      type: 'settings',
+      gameBoard: JSON.stringify(gameTiles),
+      settings: JSON.stringify(settings),
+    });
+  }
 
   // eslint-disable-next-line consistent-return
-  const importBoard = () => {
+  const importBoard = async () => {
     let gameTiles = [];
-    const entries = textValue.split('---').filter((e) => e);
+    const importLabel = textValue.match(/#.*/g).map((e) => e.replace('#', '').trim());
+    const entries = textValue
+      .split('\n')
+      .filter((line) => !line.startsWith('#')) // remove all comments.
+      .join('\n')
+      .split('---')
+      .filter((e) => e);
 
-    if (entries.length < requiredTiles) {
-      return setAlert(t('importTooShort', { entries: requiredTiles - entries.length }));
+    try {
+      if (entries.length < requiredTiles) {
+        throw setAlert(t('importTooShort', { entries: requiredTiles - entries.length }));
+      }
+      if (entries.length > requiredTiles) {
+        throw setAlert(t('importTooLong', { entries: entries.length - requiredTiles }));
+      }
+      gameTiles = entries.map((entry, index) => {
+        const [title, ...description] = entry.split('\n').filter((e) => e);
+
+        if (title === undefined) {
+          throw setAlert(t('importNoEmpty'));
+        }
+
+        const surroundedInBrackets = /^\[.+\]$/;
+        if (!surroundedInBrackets.test(title)) {
+          throw setAlert(t('importInvalidTitle', { entry: index + 1 }));
+        }
+
+        if (!description) {
+          throw setAlert(t('importMissingDescription', { entry: index + 1 }));
+        }
+
+        return { title: title?.replace(/\[|\]/g, ''), description: description.join('\n') };
+      });
+    } catch (error) {
+      // return nothing. Just wait for setAlert to output the error message.
+      return null;
     }
-    if (entries.length > requiredTiles) {
-      return setAlert(t('importTooLong', { entries: entries.length - requiredTiles }));
-    }
-    gameTiles = entries.map((entry, index) => {
-      const [title, description] = entry.split('\n').filter((e) => e);
 
-      if (title === undefined) {
-        return setAlert(t('importNoEmpty'));
-      }
-
-      const surroundedInBrackets = /^\[.+\]$/;
-      if (!surroundedInBrackets.test(title)) {
-        return setAlert(t('importInvalidTitle', { entry: index + 1 }));
-      }
-
-      if (!description) {
-        return setAlert(t('importMissingDescription', { entry: index + 1 }));
-      }
-
-      return { title: title?.replace(/\[|\]/g, ''), description };
-    });
-
+    // if we have an alert that wasn't caught by our try-catch. Should still stop importing.
     if (alert) return null;
 
-    gameTiles.unshift({ title: '', description: t('start') });
-    gameTiles.push({ title: '', description: t('finish') });
-
     setLocalGameBoard(gameTiles);
+
+    await createGameMessage(gameTiles, importLabel);
+
     close();
   };
 
   const exportBoard = () => {
     const arrayExport = localGameBoard.map(({ title, description }) => `[${title}]\n${description}`);
-    arrayExport.shift();
-    arrayExport.pop();
+    const boardString = arrayExport.join('\n---\n');
+    const stringExport = `# ${user.displayName}'s custom board\n\n${boardString}`;
 
-    const stringExport = arrayExport.join('\n---\n');
     setTextField(stringExport);
   };
 
