5058babb9f0ff7c9fbda366631eb87b6290aa79c
Shawn Dibble <shawndibble@gmail.com>
2023-10-01 19:03:34 -0500

    default difficulty curve

--- src/services/gameSettingsMessage.js
+++ src/services/gameSettingsMessage.js
@@ -56,7 +56,7 @@ function getSettingsMessage(settings, customTiles, actionsList, reason) {
 
   message += '--- \r\n';
 
-  message += `* ${t('difficulty')}: ${t(difficulty)} \r\n`;
+  message += `* ${t('difficulty')}: ${t(difficulty ?? 'normal')} \r\n`;
 
   message += `* ${t('finishSlider')} ${finishRange[0]}%  | ${finishRange[1] - finishRange[0]}% | ${100 - finishRange[1]}%`;
 
