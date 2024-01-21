aa7ee066826782a4591228a36a7896687ce42d10
Shawn Dibble <shawndibble@gmail.com>
2024-01-21 15:53:47 -0600

    fix dice roller when moving from private to public room

--- src/hooks/usePrivateRoomMonitor.js
+++ src/hooks/usePrivateRoomMonitor.js
@@ -9,26 +9,33 @@ import sendGameSettingsMessage from 'services/gameSettingsMessage';
 import { importActions } from 'services/importLocales';
 
 export default function usePrivateRoomMonitor(room, settings, gameBoard) {
+  const DEFAULT_DIEM = '1d6';
+
   const { i18n } = useTranslation();
   const { user } = useAuth();
+
   const customTiles = useLocalStorage('customTiles', [])[0];
   const { messages, isLoading } = useMessages(room);
-  const [roller, setRoller] = useState('1d4');
+  const [roller, setRoller] = useState(DEFAULT_DIEM);
   const [roomBgUrl, setRoomBackground] = useState('');
   const updateGameBoardTiles = useGameBoard();
 
-  const rebuildGameBoard = useCallback(async (messageSettings, messageUser) => {
-    const { gameMode, newBoard } = await updateGameBoardTiles(messageSettings);
+  const rebuildGameBoard = useCallback(
+    async (messageSettings, messageUser) => {
+      const { gameMode, newBoard } =
+        await updateGameBoardTiles(messageSettings);
 
-    await sendGameSettingsMessage({
-      formData: { ...settings, ...messageSettings },
-      user,
-      customTiles,
-      actionsList: importActions(i18n.resolvedLanguage, gameMode),
-      board: newBoard,
-      reason: `Rebuilt game board due to room size changes by ${messageUser}.`,
-    });
-  }, [settings, user, customTiles, i18n.resolvedLanguage, updateGameBoardTiles]);
+      await sendGameSettingsMessage({
+        formData: { ...settings, ...messageSettings },
+        user,
+        customTiles,
+        actionsList: importActions(i18n.resolvedLanguage, gameMode),
+        board: newBoard,
+        reason: `Rebuilt game board due to room size changes by ${messageUser}.`,
+      });
+    },
+    [settings, user, customTiles, i18n.resolvedLanguage, updateGameBoardTiles]
+  );
 
   useEffect(() => {
     if (isLoading) return;
@@ -37,11 +44,17 @@ export default function usePrivateRoomMonitor(room, settings, gameBoard) {
     if (roomMessage) {
       const messageSettings = JSON.parse(roomMessage.settings);
       const { roomDice, roomBackgroundURL, roomTileCount } = messageSettings;
-      setRoller(roomDice || '1d6');
+
+      let dice = DEFAULT_DIEM;
+      if (room !== 'public' && roomDice) {
+        dice = roomDice;
+      }
+
+      setRoller(dice);
       setRoomBackground(roomBackgroundURL);
 
-      const shouldRebuildGameBoard = roomMessage.uid !== user.uid
-        && roomTileCount !== gameBoard.length;
+      const shouldRebuildGameBoard =
+        roomMessage.uid !== user.uid && roomTileCount !== gameBoard.length;
 
       if (shouldRebuildGameBoard) {
         rebuildGameBoard(messageSettings, roomMessage.displayName);
@@ -51,7 +64,15 @@ export default function usePrivateRoomMonitor(room, settings, gameBoard) {
     if (settings?.roomBackgroundURL?.length) {
       setRoomBackground(settings.roomBackgroundURL);
     }
-  }, [messages, isLoading, settings, user.uid, gameBoard.length, rebuildGameBoard]);
+  }, [
+    room,
+    messages,
+    isLoading,
+    settings,
+    user.uid,
+    gameBoard.length,
+    rebuildGameBoard,
+  ]);
 
   return { roller, roomBgUrl };
 }
