67b145c2243bb74ee3d08bacaa44d3a5568072b8
Shawn Dibble <shawndibble@gmail.com>
2024-03-04 23:33:46 -0600

    clean up game guide for new players.

--- src/views/UnauthenticatedApp/index.jsx
+++ src/views/UnauthenticatedApp/index.jsx
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router-dom';
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Button,
   Card,
   CardContent,
@@ -7,7 +10,7 @@ import {
   Grid,
   Typography,
 } from '@mui/material';
-import { Language } from '@mui/icons-material';
+import { ExpandMore, Language } from '@mui/icons-material';
 import { Trans, useTranslation } from 'react-i18next';
 import Navigation from 'views/Navigation';
 import GameSettings from 'views/GameSettings';
@@ -15,12 +18,14 @@ import usePlayerList from 'hooks/usePlayerList';
 import GameGuide from 'views/GameGuide';
 import languages from 'locales/languages.json';
 import './styles.css';
+import useBreakpoint from 'hooks/useBreakpoint';
 
 export default function UnauthenticatedApp() {
   const { i18n } = useTranslation();
   const params = useParams();
   const room = params.id ?? 'PUBLIC';
   const playerList = usePlayerList(room);
+  const isMobile = useBreakpoint('sm');
 
   const languageLinks = Object.entries(languages).map(([key, obj]) => (
     <Button
@@ -37,6 +42,24 @@ export default function UnauthenticatedApp() {
       <Navigation room={room} playerList={playerList} />
       <Container maxWidth='lg' sx={{ mt: 8 }}>
         <Grid container>
+          <Grid item xs={12} sm={6} md={4} sx={{ mx: isMobile && 0.5 }}>
+            {isMobile ? (
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMore />} >
+                  <Typography variant='h6'>Game Guide:</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <GameGuide />
+                </AccordionDetails>
+              </Accordion>
+            ) : (
+              <Card className='unauthenticated-card'>
+                <CardContent>
+                  <GameGuide />
+                </CardContent>
+              </Card>
+            )}
+          </Grid>
           <Grid item xs={12} sm={6} md={8}>
             <Card className='unauthenticated-card'>
               <CardContent>
@@ -47,19 +70,12 @@ export default function UnauthenticatedApp() {
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card className='unauthenticated-card'>
-              <CardContent>
-                <GameGuide />
-              </CardContent>
-            </Card>
-          </Grid>
         </Grid>
         <Grid container sx={{ mt: 1 }}>
           <Grid item className='language'>
             <Card className='unauthenticated-card'>
               <CardContent className='translation-card-content'>
-                <Language sx={{ mr: 1 }} />
+                {!isMobile && <Language sx={{ mr: 1 }} />}
                 <Typography sx={{ mr: 1 }}>
                   <Trans i18nKey='language' />:
                 </Typography>
