7ee45d0cfd104b1646be618f32a12cb1ec96cd38
Shawn Dibble <shawndibble@gmail.com>
2024-03-24 09:39:34 -0500

    drop bating as default. Force people to actually pick something.

--- src/views/GameSettings/index.jsx
+++ src/views/GameSettings/index.jsx
@@ -44,7 +44,6 @@ export default function GameSettings({ submitText, closeDialog }) {
     finishRange: [30, 70],
     roomTileCount: 40,
     roomDice: '1d6',
-    bating: {level: 1},
   });
   const navigate = useNavigate();
 
