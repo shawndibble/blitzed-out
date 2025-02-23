c0346198de65849c5bfa18b485a07ebd8b04a1b6
Shawn Dibble <shawndibble@gmail.com>
2025-02-23 00:18:30 -0600

    add react compiler for performance

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [["babel-plugin-react-compiler"]]
+    }
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Use '@' as an alias for 'src'
