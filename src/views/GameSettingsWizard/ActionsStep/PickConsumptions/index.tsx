252a7ae53ac3c2c94aed50bb46801fcf21978c9b
Shawn Dibble <shawndibble@gmail.com>
2025-03-18 16:28:19 -0500

    feat: Add game settings wizard, room, and game boards management views

---
+++ src/views/GameSettingsWizard/ActionsStep/PickConsumptions/index.tsx
@@ -0,0 +1,94 @@
+import { Typography } from '@mui/material';
+import IncrementalSelect from '@/components/GameForm/IncrementalSelect';
+import YesNoSwitch from '@/components/GameForm/YesNoSwitch';
+import { useState } from 'react';
+import { Trans } from 'react-i18next';
+import IntensityTitle from '../IntensityTitle';
+import { populateSelections, handleChange, updateFormDataWithDefaults } from '../helpers';
+import MultiSelect from '@/components/MultiSelect';
+
+const MAX_CONSUME = 2;
+
+export default function PickConsumptions({ formData, setFormData, options, actionsList }) {
+  const action = 'consumption';
+  const optionList = options(action);
+
+  const initialConsumptions = populateSelections(formData, optionList, action);
+  const [selectedConsumptions, setSelectedConsumptions] = useState(initialConsumptions);
+
+  const handleConsumptionChange = (event) => {
+    const { value } = event.target;
+
+    if (value.length <= MAX_CONSUME) {
+      setSelectedConsumptions(value);
+      updateFormDataWithDefaults(value, action, setFormData);
+    }
+  };
+
+  const variationChange = (event, selectedItems) => {
+    const updatedFormData = selectedItems.reduce(
+      (acc, option) => {
+        acc[option] = {
+          ...acc[option],
+          type: action,
+          variation: event.target.checked ? 'appendMost' : 'standalone',
+        };
+        return acc;
+      },
+      { ...formData, isAppend: event.target.checked }
+    );
+
+    setFormData(updatedFormData);
+  };
+
+  return (
+    <>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        <Trans i18nKey="pickConsumptions" />
+      </Typography>
+
+      <MultiSelect
+        onChange={handleConsumptionChange}
+        values={selectedConsumptions}
+        options={optionList}
+        label={<Trans i18nKey="consumables" />}
+      />
+
+      {!!selectedConsumptions.length && (
+        <>
+          <IntensityTitle />
+
+          {selectedConsumptions.map((option) => (
+            <IncrementalSelect
+              key={option}
+              actionsFolder={actionsList}
+              settings={formData}
+              option={option}
+              initValue={1}
+              onChange={(event) =>
+                handleChange(
+                  event,
+                  option,
+                  action,
+                  setFormData,
+                  setSelectedConsumptions,
+                  formData.isAppend ? 'appendMost' : 'standalone'
+                )
+              }
+            />
+          ))}
+
+          <Typography variant="h6" sx={{ mt: 2 }}>
+            <Trans i18nKey="standaloneOrCombine" />
+          </Typography>
+
+          <YesNoSwitch
+            trueCondition={formData.isAppend}
+            onChange={(event) => variationChange(event, selectedConsumptions)}
+            yesLabel="combineWithActions"
+          />
+        </>
+      )}
+    </>
+  );
+}
---
+++ src/views/GameSettingsWizard/index.tsx
@@ -0,0 +1,128 @@
+import { useEffect, useRef, useState } from 'react';
+import { Box, Button, CircularProgress, Divider, Step, StepLabel, Stepper } from '@mui/material';
+import { Trans } from 'react-i18next';
+import RoomStep from './RoomStep';
+import GameModeStep from './GameModeStep';
+import ActionsStep from './ActionsStep';
+import FinishStep from './FinishStep';
+import GameSettings from '@/views/GameSettings';
+import { useParams } from 'react-router-dom';
+import useSettingsToFormData from '@/hooks/useSettingsToFormData';
+import { isPublicRoom } from '@/helpers/strings';
+import useActionList from '@/hooks/useActionList';
+
+export default function GameSettingsWizard({ close }) {
+  const { id: room } = useParams();
+  const [step, setStep] = useState(1);
+
+  const overrideSettings = { room };
+  if (isPublicRoom(room)) {
+    // if we are in the public room, some settings are forced.
+    overrideSettings.gameMode = 'online';
+    overrideSettings.roomRealtime = true;
+  }
+
+  const [formData, setFormData] = useSettingsToFormData(
+    {
+      gameMode: 'online',
+      roomRealtime: true,
+      actions: [],
+      consumption: [],
+      role: 'sub',
+    },
+    overrideSettings
+  );
+
+  const initialLoad = useRef(true);
+  const { actionsList } = useActionList(formData.gameMode);
+
+  // on load, we want to guess what page we should be on.
+  useEffect(() => {
+    if (!initialLoad.current) return;
+    initialLoad.current = false;
+
+    if (formData.advancedSettings) return goToAdvanced();
+
+    // if we do not have a close() then this dialog opened b/c we need to set up a game.
+    // this also means we need to do all steps.
+    if (typeof close !== 'function') return;
+
+    // If we are in the public room, then we can skip to step 3.
+    if (isPublicRoom(room)) return setStep(3);
+
+    setStep(2);
+  }, [formData]);
+
+  const nextStep = (count) => {
+    if (!Number.isInteger(count)) return setStep(step + 1);
+    setStep(step + count);
+  };
+
+  const prevStep = (count) => {
+    if (!Number.isInteger(count)) return setStep(step - 1);
+    setStep(step - count);
+  };
+
+  const goToAdvanced = () => setStep('advanced');
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <RoomStep formData={formData} setFormData={setFormData} nextStep={nextStep} />;
+      case 2:
+        return (
+          <GameModeStep
+            formData={formData}
+            setFormData={setFormData}
+            nextStep={nextStep}
+            prevStep={prevStep}
+          />
+        );
+      case 3:
+        return (
+          <ActionsStep
+            formData={formData}
+            setFormData={setFormData}
+            nextStep={nextStep}
+            prevStep={prevStep}
+            actionsList={actionsList}
+          />
+        );
+      case 4:
+        return (
+          <FinishStep
+            formData={formData}
+            setFormData={setFormData}
+            prevStep={prevStep}
+            actionsList={actionsList}
+            close={close}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  if (step === 'advanced') return <GameSettings closeDialog={close} />;
+
+  return (
+    <Box>
+      <Box sx={{ width: '100%', mt: 2, mb: 4 }}>
+        <Stepper activeStep={step - 1} alternativeLabel>
+          {[1, 2, 3, 4].map((label) => (
+            <Step key={label}>
+              <StepLabel />
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+      {renderStep()}
+
+      <Divider sx={{ mt: 3 }} />
+
+      <Button onClick={goToAdvanced}>
+        <Trans i18nKey="advancedSetup" />
+      </Button>
+    </Box>
+  );
+}
