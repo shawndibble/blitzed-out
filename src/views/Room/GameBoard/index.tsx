0529a26bbe6987078bff01a24f44bb281f7e7625
Shawn Dibble <shawn@redcatsystems.com>
2025-08-01 12:36:33 -0500

    refactor scrollbar styling to reusable class

--- src/views/Room/GameBoard/index.tsx
+++ src/views/Room/GameBoard/index.tsx
@@ -79,7 +79,7 @@ export default function GameBoard({
   });
 
   return (
-    <div className="gameboard">
+    <div className="gameboard transparent-scrollbar">
       <ol>{gameTiles}</ol>
     </div>
   );
