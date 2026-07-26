a7cd5c3d3cd39053aef0d856b396b06a1a35c1a2
Shawn Dibble <shawn@redcatsystems.com>
2026-07-26 16:47:55 -0500

    Updates

---
+++ js/index.tsx-sa-bysAe.js
@@ -0,0 +1,2 @@
+import{an as o}from"./chunk-mbcHQzXL.js";import{t as n}from"./chunk-BL5Hyi4e.js";import{v as s}from"./chunk-BN02bVHy.js";import{t}from"./chunk-CGS-BiXL2.js";var a=o();function r({open:o,close:r=null}){const[i]=t(),{isLoading:e,actionsList:u}=s(null==i?void 0:i.gameMode);return e?null:(0,a.jsx)(n,{setOpen:r||(()=>{}),boardUpdated:()=>null,actionsList:u,open:o})}export{r as default};
+//# sourceMappingURL=index.tsx-sa-bysAe.js.map
